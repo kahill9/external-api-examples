749d64626f74baceca6c8ddad51b44cd33ccfdfd
Hill <kaylie.hill@cgi.com>
2025-06-05 11:43:04 -0500

    weather forecast example

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import sunLogo from "./assets/sun.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todaysWeatherForecast, setTodaysWeatherForecast] = useState();
+  const [isTodayVisible, setIsTodayVisible] = useState(false);
+
+  const [tomorrowsWeatherForecast, setTomorrowsWeatherForecast] = useState();
+  const [isTomorrowVisible, setIsTomorrowVisible] = useState(false);
+
+  useEffect(() => {}, []); // Empty dependency array, runs only once on initial load
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch(
+        "https://api.weather.gov/gridpoints/BMX/59,85/forecast"
+      );
+      const data = await response.json();
+
+      setTodaysWeatherForecast(
+        data.properties.periods.find((obj) => obj.name === "Today")
+      );
+      setIsTodayVisible(true);
+
+      setTomorrowsWeatherForecast(
+        data.properties.periods.find((obj) => obj.number === 3)
+      );
+      setIsTomorrowVisible(true);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={sunLogo} className="logo sun" alt="Sun logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Weather Forecast</h1>
+      <div>
+        <p>Click this button to load weather data for Birmingham, AL!</p>
+        <button onClick={handleClick}>Fetch Weather Data</button>
+
+        <div id="todaysWeatherForcast">
+          {todaysWeatherForecast && (
+            <>
+              {isTodayVisible ? (
+                <div class="card">
+                  <h1 class="temperature">
+                    {todaysWeatherForecast.temperature}
+                  </h1>
+                  <div class="container">
+                    <h4>
+                      <b>Today's Forecast</b>
+                    </h4>
+                    <p>{todaysWeatherForecast.detailedForecast}</p>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+            </>
+          )}
+        </div>
+
+        <div id="tomorrowsWeatherForecast">
+          {tomorrowsWeatherForecast && (
+            <>
+              {isTomorrowVisible ? (
+                <div class="card">
+                  <h1 class="temperature">
+                    {tomorrowsWeatherForecast.temperature}
+                  </h1>
+                  <div class="container">
+                    <h4>
+                      <b>Tomorrow's Forecast</b>
+                    </h4>
+                    <p>{tomorrowsWeatherForecast.detailedForecast}</p>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+            </>
+          )}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
